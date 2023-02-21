da1557134eab7978c42f3879ab1c12627bbb6663
Mehulbapse <bapsemehul123@gmail.com>
2023-02-21 10:43:11 +0530

    Checkout Page

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,13 @@
 import './product-card.styles.scss';
-
+import { useContext } from 'react';
 import Button from '../button/button.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const ProductCard = ({ product }) => {
+  const {addItemToCart} = useContext(CartContext)
+
   const { name, price, imageUrl } = product;
+  const addProducttoCart =() => addItemToCart(product)
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +15,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to card</Button>
+      <Button buttonType='inverted' onClick={addProducttoCart}>Add to card</Button>
     </div>
   );
 };
